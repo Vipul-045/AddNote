d2164ab33d59f1b065458281d5177be5b006f053
Vipul Yadav <vipulyadav0479@gmail.com>
2023-07-23 21:42:11 +0530

    Added a few lines of code

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,31 @@
 console.log('Welcome Vipul!!')
+
+// If user adds a note, add it to the localStorage
+let Addbtn = document.getElementById('Addbtn');
+Addbtn.addEventListener("click",function(e){
+
+let Addtxt = document.getElementById("Addtxt");
+let notes = localStorage.getItem("notes");
+if(notes==null){
+    notesObj=[];
+}
+else{
+    notesObj = JSON.parse(notes);
+}
+notesObj.push(Addtxt.value);
+localStorage.setItem("notes", JSON.stringify(notesObj));
+Addtxt.value="";
+console.log(notesObj, localStorage.length);
+showNotes();
+
+})
+
+function showNotes() {
+    let notes = localStorage.getItem("notes");
+    if(notes == null){
+        notesObj=[];
+    }
+    else{
+        notesObj = JSON.parse(notes);
+    }
+}
