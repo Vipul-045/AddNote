e8f805ca3295bc67febeaf0c692d885b87a964a0
Vipul Yadav <vipulyadav0479@gmail.com>
2023-07-24 21:11:48 +0530

    Done with few lines of code

--- JS/app.js
+++ JS/app.js
@@ -1,31 +1,51 @@
 console.log('Welcome Vipul!!')
-
-// If user adds a note, add it to the localStorage
-let Addbtn = document.getElementById('Addbtn');
-Addbtn.addEventListener("click",function(e){
-
-let Addtxt = document.getElementById("Addtxt");
-let notes = localStorage.getItem("notes");
-if(notes==null){
-    notesObj=[];
-}
-else{
-    notesObj = JSON.parse(notes);
-}
-notesObj.push(Addtxt.value);
-localStorage.setItem("notes", JSON.stringify(notesObj));
-Addtxt.value="";
-console.log(notesObj, localStorage.length);
 showNotes();
 
-})
+// If user adds a note, add it to the localStorage
+let addBtn = document.getElementById("addBtn");
+addBtn.addEventListener("click", (e) => {
+
+    let Addtxt = document.getElementById("Addtxt");
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.push(Addtxt.value);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    Addtxt.value = "";
+    console.log(notesObj, localStorage.length);
+    showNotes();
+
+});
 
 function showNotes() {
     let notes = localStorage.getItem("notes");
-    if(notes == null){
-        notesObj=[];
+    if (notes == null) {
+        notesObj = [];
     }
-    else{
+    else {
         notesObj = JSON.parse(notes);
     }
+
+    let html = "";
+    notesObj.forEach(function (element, index) {
+        html += `
+        <div class="row container-fluid" id="notes">
+            <div class="my-2 mx-2 card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Note $(index+1)</h5>
+                    <p class="card-text">${element}</p>
+                    <button href="#" class="btn btn-primary" z-index=1> Delete Note</button>
+                </div>
+                </div>
+                </div>;`
+    });
+    let notesElm = document.getElementById('notes');
+    if (notesObj.length != 0) {
+        notesElm.innerHTML = html;
+    }
+
 }
