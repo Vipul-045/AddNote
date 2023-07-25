98f9d7ee6c24d3d2ec20fdfe1dc13554c401c03b
Vipul Yadav <vipulyadav0479@gmail.com>
2023-07-25 22:00:29 +0530

    Added a feature to delete a note

--- JS/app.js
+++ JS/app.js
@@ -21,6 +21,7 @@ addBtn.addEventListener("click", (e) => {
 
 });
 
+// Function to show elements from localstorage
 function showNotes() {
     let notes = localStorage.getItem("notes");
     if (notes == null) {
@@ -37,7 +38,7 @@ function showNotes() {
                 <div class="card-body">
                     <h5 class="card-title">Note ${index+1}</h5>
                     <p class="card-text">${element}</p>
-                    <button href="#" class="btn btn-primary" z-index=1> Delete Note</button>
+                    <button id="${index}" onclick="deletenote(this.id)" class="btn btn-primary"> Delete Note</button>
                 </div>
                 </div>`;
     });
@@ -48,5 +49,22 @@ function showNotes() {
     else {
         notesElm.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes`
     }
+    
+}
 
+// Function to delete a note
+function deletenote(index){
+    console.log('I am deleting', index);
+
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+
+    notesObj.splice(index, 1);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    showNotes();
 }
