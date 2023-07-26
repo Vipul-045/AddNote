b9ea77b258e0901931a7ead00d4dca5bfb25542b
Vipul Yadav <vipulyadav0479@gmail.com>
2023-07-26 21:01:14 +0530

    Added a search button

--- JS/app.js
+++ JS/app.js
@@ -16,7 +16,7 @@ addBtn.addEventListener("click", (e) => {
     notesObj.push(Addtxt.value);
     localStorage.setItem("notes", JSON.stringify(notesObj));
     Addtxt.value = "";
-    console.log(notesObj, localStorage.length);
+    // console.log(notesObj, localStorage.length);
     showNotes();
 
 });
@@ -34,7 +34,7 @@ function showNotes() {
     let html = "";
     notesObj.forEach(function (element, index) {
         html += `
-            <div class="my-2 mx-2 card" style="width: 18rem;">
+            <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
                 <div class="card-body">
                     <h5 class="card-title">Note ${index+1}</h5>
                     <p class="card-text">${element}</p>
@@ -54,7 +54,7 @@ function showNotes() {
 
 // Function to delete a note
 function deletenote(index){
-    console.log('I am deleting', index);
+    // console.log('I am deleting', index);
 
     let notes = localStorage.getItem("notes");
     if (notes == null) {
@@ -68,3 +68,22 @@ function deletenote(index){
     localStorage.setItem("notes", JSON.stringify(notesObj));
     showNotes();
 }
+
+let search = document.getElementById('searchTxt');
+search.addEventListener('input', function(){
+
+    let inputVal = search.value.toLowerCase();
+    // console.log('Input event fired', inputVal);
+    let noteCards = document.getElementsByClassName('noteCard');
+    Array.from(noteCards).forEach(function(element){
+        let cardTxt = element.getElementsByTagName("p")[0].innerText;
+        if(cardTxt.includes(inputVal)){
+            element.style.display = "block";
+        }
+        else{
+            element.style.display = "none";
+        }
+        // console.log(cardTxt);
+    })
+
+})
